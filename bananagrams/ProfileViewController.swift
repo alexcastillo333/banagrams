186350a1e992f08814d7455c5cd0006f7f8cefa8
Aaron Posadas <atposadas54@gmail.com>
2024-03-18 14:43:16 -0500

    add splash screen, bananagram logo, and profile alpha framework

---
+++ bananagrams/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  bananagrams
+//
+//  Created by Aaron Posadas on 3/18/24.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
