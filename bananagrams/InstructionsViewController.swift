072fddd42be29142b0c78452478da46f9851df0d
Taqi Hossain <taqi.hossain@gmail.com>
2024-03-18 16:31:19 -0500

    added instructionsand color touch up

--- bananagrams/InstructionsViewController.swift
+++ bananagrams/InstructionsViewController.swift
@@ -7,9 +7,9 @@
 
 import UIKit
 
-let freeplay = "Placeholder for Freeplay instructions"
-let timed = "Placeholder for Timed instructions"
-let versus = "Placeholder for Versus instructions"
+let freeplay = "drag and drop your letters onto the grid zoom in and out by pinching create one big crossword with your letters peel when you have used all your letters to receive a random letter shake to dump the last letter you placed and receive 3 random letters keep peeling until there are no letters left!"
+let timed = "drag and drop your letters onto the grid zoom in and out by pinching create one big crossword with your letters peel when you have used all your letters to receive a new letter shake to dump the last letter you placed and receive 3 new letters keep peeling until there are no letters left race against the clock!"
+let versus = "drag and drop your tiles onto the gridzoom in and out by pinchingcreate one big crossword with your letterspeel when you have used all your letters to give a new letter to yourself and your opponentsshake to dump the last letter you placed and receive 3 new letterskeep peeling until there are no letters left! the last player to peel wins!"
 class InstructionsViewController: UIViewController {
 
     override func viewDidLoad() {
